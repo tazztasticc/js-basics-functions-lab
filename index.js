6ac72f01243215cb3ef45698e19d0914cb40bcb6
Tasif <taz181995@gmail.com>
2021-05-19 17:58:56 -0400

    commit tree d1927c47a712bde72c361a7bcb62fe54eb1d3eb2

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+
+function distanceFromHqInBlocks(someValue){
+    if(someValue > 42){
+        return someValue - 42;
+    }
+    else return 42- someValue;
+}
+
+function distanceFromHqInFeet(someValue){
+    return distanceFromHqInBlocks(someValue) * 264;
+}
+
+function distanceTravelledInFeet(valueA,valueB){
+    if (valueA>valueB){
+        return (valueA-valueB)*264;
+    }
+    else return (valueB-valueA)*264;
+}
+
+function calculatesFarePrice(valueA,valueB){
+    let distance = distanceTravelledInFeet(valueA,valueB);
+    if(distance < 400){
+        return 0;
+    }
+    
+    if (distance >2500){
+        return 'cannot travel that far';
+    }
+    if (distance>2000){
+        return 25;
+    }
+    else return (distance - 400) * .02;
+}
+
